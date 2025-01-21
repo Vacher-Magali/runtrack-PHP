d752599c8e56deaa2ced91b927624edcf017e592
Vacher-Magali <magali.vacher@laplateforme.io>
2025-01-21 12:54:52 +0100

    langue fr

--- jour07/morpion/index.php
+++ jour07/morpion/index.php
@@ -56,7 +56,7 @@ if (isset($_POST['row']) && isset($_POST['col']) && $_SESSION['winner'] === null
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
